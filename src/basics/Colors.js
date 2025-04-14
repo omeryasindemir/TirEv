3b667934015984f8906d08b702f330b9c3265d5a
omeryasind7 <omeryasindemir485@gmail.com>
2025-04-14 19:16:47 +0300

    welcome header and colors

--- src/basics/Colors.js
+++ src/basics/Colors.js
@@ -3,5 +3,9 @@ export default Colors = {
     box: "#393F4C",
     gray: "#31353E",
     gray2: "#272B35",
-    main: "#00aaff"
+    main: "#00aaff",
+    white: "#fff",
+    black: "#000",
+    border: "#50535B",
+    text: "#cccccc"
 }
--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -1,24 +1,79 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import CustomText from "../components/CustomText"
+import Colors from '../basics/Colors';
 
 export default function Welcome({ navigation }) {
   return (
     <View style={styles.container}>
-      <CustomText style={styles.title}>Welcome to My App!</CustomText>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
+      <View style={{
+        width: "100%",
+        maxWidth: "1280px",
+        flex: 1
+      }}>
+        <View style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          display: "flex"
+        }}>
+          <View>
+          <CustomText style={{
+              ...styles.text,
+              color: Colors.text,
+              marginBottom: 0
+            }}>Evine Hoşgeldin</CustomText>
+            <CustomText style={{
+              ...styles.text,
+              fontSize: 20
+            }}>Selam, <span style={{ fontWeight: "bold" }}>Nurefşan</span> ✌️</CustomText>
+
+          </View>
+          <View style={{
+            height: 50,
+            width: 50,
+            borderRadius: "50%",
+            borderWidth: 1,
+            borderColor: Colors.border,
+            backgroundImage: `linear-gradient(-45deg, ${Colors.border}, ${Colors.gray})`,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            overflow: "hidden"
+          }}>
+            <View style={{
+              height: 20,
+              width: 20,
+              borderRadius: "50%",
+              backgroundImage: `linear-gradient(-45deg, ${Colors.white}, ${Colors.border})`,
+              marginBottom: 4,
+              marginTop: -8
+            }}></View>
+            <View style={{
+              height: 40,
+              width: 40,
+              borderRadius: "50%",
+              backgroundImage: `linear-gradient(-45deg, ${Colors.white}, ${Colors.border})`,
+              marginBottom: -40
+            }}></View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, alignItems: 'center', justifyContent: 'center',
+    backgroundImage: `linear-gradient(-45deg, ${Colors.background}, ${Colors.gray})`,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 24,
+    paddingHorizontal: 32
   },
-  title: {
-    fontSize: 24, fontWeight: 'bold', marginBottom: 20,
+  text: {
+    fontSize: 12,
+    color: Colors.white
   },
 });
