855eca8f318590efb46285e5df8d5e863cfc1a88
omeryasind7 <omeryasindemir485@gmail.com>
2025-04-14 20:31:48 +0300

    fix

--- src/routes/Welcome.js
+++ src/routes/Welcome.js
@@ -184,7 +184,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    paddingVertical: 16
+    paddingVertical: 24
   },
   text: {
     fontSize: 12,
